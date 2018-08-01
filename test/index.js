6c09da6ca070446ba3a58c7287d679c8c24f3856
Jonathan Haines <jonno.haines@gmail.com>
2018-08-01 10:05:01 +1000

    enable prettier

--- test/index.js
+++ test/index.js
@@ -14,31 +14,41 @@ var ROOT = join(__dirname, 'fixtures');
 
 var fixtures = fs.readdirSync(ROOT);
 
-test('mdast-util-toc()', function (t) {
+test('mdast-util-toc()', function(t) {
     t.is(typeof toc, 'function', 'should be a function');
 
-    t.throws(function () {
-        toc();
-    }, 'Cannot read property \'children\' of undefined', 'should fail without node');
+    t.throws(
+        function() {
+            toc();
+        },
+        "Cannot read property 'children' of undefined",
+        'should fail without node'
+    );
 
     t.end();
 });
 
-test('Fixtures', function (t) {
-    fixtures.filter(function (filepath) {
-        return filepath.indexOf('.') !== 0;
-    }).forEach(function (fixture) {
-        var filepath = join(ROOT, fixture);
-        var output = JSON.parse(read(join(filepath, 'output.json'), 'utf8'));
-        var input = remark().use(remarkAttr).parse(read(join(filepath, 'input.md'), 'utf-8'));
-        var config = join(filepath, 'config.json');
-        var result;
+test('Fixtures', function(t) {
+    fixtures
+        .filter(function(filepath) {
+            return filepath.indexOf('.') !== 0;
+        })
+        .forEach(function(fixture) {
+            var filepath = join(ROOT, fixture);
+            var output = JSON.parse(
+                read(join(filepath, 'output.json'), 'utf8')
+            );
+            var input = remark()
+                .use(remarkAttr)
+                .parse(read(join(filepath, 'input.md'), 'utf-8'));
+            var config = join(filepath, 'config.json');
+            var result;
 
-        config = exists(config) ? JSON.parse(read(config, 'utf-8')) : {};
-        result = toc(input, config);
+            config = exists(config) ? JSON.parse(read(config, 'utf-8')) : {};
+            result = toc(input, config);
 
-        t.deepEqual(result, output, 'should work on `' + fixture + '`');
-    });
+            t.deepEqual(result, output, 'should work on `' + fixture + '`');
+        });
 
     t.end();
 });
