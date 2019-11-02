2f4893989da57c1e0072cd4898543469d4317297
Jonathan Haines <jonno.haines@gmail.com>
2019-11-03 06:46:54 +1100

    Add docs to TOCOptions

--- types/index.d.ts
+++ types/index.d.ts
@@ -7,18 +7,53 @@ import {Test} from 'unist-util-is'
 declare namespace mdastUtilToc {
 
   interface TOCOptions {
+    /**
+     * Heading to look for, wrapped in `new RegExp('^(' + value + ')$', 'i')`.
+     */
     heading?: string
+
+    /**
+     * Maximum heading depth to include in the table of contents (default: `6`),
+     * This is inclusive: when set to `3`,
+     * level three headings are included (those with three hashes, `###`).
+     */
     maxDepth?: Heading['depth']
+
+    /**
+     * Headings to skip, wrapped in `new RegExp('^(' + value + ')$', 'i')`.
+     * Any heading matching this expression will not be present in the table of contents.
+     */
     skip?: string
+
+    /**
+     * Whether to compile list-items tightly (default: `false`).
+     */
     tight?: boolean
+
+    /**
+     * Add a prefix to links to headings in the table of contents (default: null).
+     * Useful for example when later going from mdast to hast and sanitizing with `hast-util-sanitize`.
+     */
     prefix?: string
+
+    /**
+     * Allows headings to be children of certain node types
+     * (default: the to `toc` given `tree`, to only allow top-level headings).
+     * Internally, uses `unist-util-is` to check, so `parents` can be any `is`-compatible test.
+     *
+     * For example, this would allow headings under either `root` or `blockquote` to be used:
+     *
+     * ```ts
+     * toc(tree, {parents: ['root', 'blockquote']})
+     * ```
+     */
     parents?: Test<Node> | Array<Test<Node>>
   }
 
   interface TOCResult {
-    index?: number
-    endIndex?: number
-    map?: List
+    index: number | null
+    endIndex: number | null
+    map: List | null
   }
 }
 
